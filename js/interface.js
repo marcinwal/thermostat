d69e7d35f7799ba4a5263c21c98e70176786c054
marcin walendzik <marcin.walendzik@gmail.com>
2015-01-21 17:31:53 +0000

    colored indicator

--- js/interface.js
+++ js/interface.js
@@ -1,15 +1,25 @@
 console.log("Hi Demkiw!");
 
 var thermostat = new Thermostat();
+var tempColor;
 
 var updateTemperature = function(){
-  $('#temperature').text(thermostat.temperature);
-     if (thermostat.savingMode){
-         $('.PWS-mode').css('background','green').text("POWER SAVE ON");
-     }else
-     {
-         $('.PWS-mode').css('background','red').text("POWER SAVE OFF");
-     } 
+
+  switch(thermostat.consoleColor()){
+    case 'lowUsage' : tempColor = "green";break;
+    case 'mediumUsage' : tempColor = "yellow";break;
+    case 'highUsage' :tempColor = "red";break;
+  }
+  
+  $('#temperature').css('color',tempColor).text(thermostat.temperature);
+  if (thermostat.savingMode){
+      $('.PWS-mode').css('background','green').text("POWER SAVE ON");
+  }else
+  {
+      $('.PWS-mode').css('background','red').text("POWER SAVE OFF");
+  }
+
+
 };
 
 $(document).ready(function(){
